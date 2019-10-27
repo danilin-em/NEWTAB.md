fbeb470e6dae57a2dd00ed4e6bd6c9cb80b74229
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2019-10-27 21:48:37 +0400

    Add watch task

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,8 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'src'),
     },
+    watch: true,
+    watchOptions: {
+        ignored: '/node_modules/',
+    },
 };

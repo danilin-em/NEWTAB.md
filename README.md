403d53da98f7176851a04c610167ac8acfb28e2c
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2019-10-27 18:00:28 +0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# NEWTAB.md
---
+++ src/js/bookmarks.js
@@ -0,0 +1,40 @@
+var $ = require("jquery");
+
+function dumpBookmarks(query) {
+    chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+        console.log('bookmarkTreeNodes>>>', bookmarkTreeNodes);
+        $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes[0].children, 'root'));
+    });
+}
+
+function dumpTreeNodes(bookmarkNodes, stage) {
+    const list = $('<ul class="list list-'+stage+'">');
+    let i;
+    for (i = 0; i < bookmarkNodes.length; i++) {
+        list.append(dumpNode(bookmarkNodes[i], stage));
+    }
+    return list;
+}
+
+function dumpNode(bookmarkNode, stage) {
+    let anchor = $('<span>').text('X');
+    if (bookmarkNode.title) {
+        anchor = $('<a>');
+        anchor.attr('href', bookmarkNode.url);
+        anchor.text(bookmarkNode.title);
+        anchor.click(function() {
+            chrome.tabs.create({url: bookmarkNode.url});
+        });
+    }
+    const span = $('<span>');
+    span.append(anchor);
+    const li = $('<li class="item item-'+stage+'">').append(span);
+    if (bookmarkNode.children && bookmarkNode.children.length > 0) {
+        li.append(dumpTreeNodes(bookmarkNode.children, 'children'));
+    }
+    return li;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    dumpBookmarks();
+});
